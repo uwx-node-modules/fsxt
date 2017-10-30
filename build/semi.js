87b569ff6c95cca5de1938df146bee38ba9c2b69
uwx <cadrekucra@gmx.com>
2017-10-30 00:58:14 -0200

    update semi script

--- build/semi.js
+++ build/semi.js
@@ -4,6 +4,7 @@ const esformatter = require('esformatter');
 const fs = require('..');
 
 for (let file of fs.diveSync('./lib')) {
+  if (!file.endsWith('.js')) continue;
   try {
     fs.writeFileSync('./' + file, 
       esformatter.format(fs.readFileSync('./' + file, 'utf8'), {
