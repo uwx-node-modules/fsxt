bd357cae4a4defeb3f88359cc82c1cba06fd8ba4
uwx <uwx@users.noreply.github.com>
2024-02-09 01:14:18 -0300

    Add missing 'clobber' option

--- lib/move/index.d.ts
+++ lib/move/index.d.ts
@@ -1,3 +1,11 @@
 export { move, moveSync } from 'fs-extra';
+import type { NoParamCallbackWithUndefined, MoveOptions } from 'fs-extra';
+
+interface MoveOptions {
+    clobber?: boolean;
+}
 
 export function move(src: string, dest: string, options: undefined, callback: NoParamCallbackWithUndefined): void;
+export function move(src: string, dest: string, options?: MoveOptions): Promise<void>;
+export function move(src: string, dest: string, callback: NoParamCallbackWithUndefined): void;
+export function move(src: string, dest: string, options: MoveOptions, callback: NoParamCallbackWithUndefined): void;
